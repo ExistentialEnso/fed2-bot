b27e0254b1b3c61fd26e37555cdcc9b375ee903e
Thorne Melcher <existentialenso@gmail.com>
2020-03-22 04:58:08 -0400

    Break out validation functions into their own file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Telnet = require('telnet-client')
 const _ = require('lodash')
 const chalk = require('chalk')
 const emojiMap = require('./emojiMap')
+const validator = require('./lib/validator')
 
 // Load in our .env file
 require('dotenv').config()
@@ -17,6 +18,13 @@ const SLEEP_MINUTES = 15
 
 // How low should stamina go before we buy food?
 const STAMINA_MIN = 25
+
+// Information for connection to Fed2's servers
+const connectionParams = {
+    host: 'play.federation2.com',
+    port: 30003,
+    negotiationMandatory: false
+}
 
 // Useful regexes for pulling out data from game output
 let commodRegex = new RegExp("([A-Z,a-z]*): value ([0-9]*)ig/ton  Spread: ([0-9]*)%   Stock: current ([-0-9]*)")
@@ -49,24 +57,21 @@ function sleep(ms) {
  * The main function that starts up everything else
  */
 async function run() {
-    let params = {
-        host: 'play.federation2.com',
-        port: 30003,
-        negotiationMandatory: false
+    console.log("Validating steps & planet data for errors...")
+    const stepsValid = validator.validateSteps(steps)
+    const planetValid = validator.validatePlanets(planets)
+
+    if(!planetValid || !stepsValid) {
+        console.log(chalk.red("Please fix the errors with your configuration and run again."))
+        process.exit(0)
     }
 
-    console.log("Validating steps data for errors.")
-    validateSteps(steps)
-    console.log(chalk.green("No issues found!"))
-
-    console.log("Validating planets data for errors.")
-    validatePlanets(planets)
     console.log(chalk.green("No issues found!"))
 
     console.log("Connecting to Federation 2 servers...")
 
     try {
-        await connection.connect(params)
+        await connection.connect(connectionParams)
     } catch(error) {
         console.log(chalk.red("ERROR! Could not connect to Fed2."))
         process.exit(0)
@@ -89,101 +94,6 @@ async function run() {
 
         // Sleep until we need to repeat the cycle again
         await sleep(1000 * 60 * SLEEP_MINUTES)
-    }
-}
-
-/**
- * Ensure the steps.js file doesn't have obvious errors
- */
-function validateSteps() {
-    let startingPlanet = steps[0].from
-    let previousEndsAt = "NONE"
-
-    let valid = true
-
-    for(let step of steps) {
-        if(!step.from || !step.to || step.planet) {
-            console.log(chalk.red("ERROR! This step is missing required field(s): " + JSON.stringify(step)))
-            valid = false
-        }
-
-        if(step.type !== "TRADE" && step.type !== "MOVE") {
-            console.log(chalk.red("ERROR! Invalid step type: " + step.type))
-            valid = false
-        }
-
-        if(step.from === step.to) {
-            console.log(chalk.red("ERROR! A step can't have the same from & to values: " + JSON.stringify(step)))
-        }
-
-        if(previousEndsAt !== "NONE" && previousEndsAt !== step.from) {
-            console.log(chalk.red("ERROR! Previous step ended at a different planet: " + JSON.stringify(step)))
-            valid = false
-        }
-
-        if(step.type === "TRADE") {
-            previousEndsAt = step.from
-        } else {
-            previousEndsAt = step.to
-        }
-    }
-
-    if(startingPlanet !== previousEndsAt) {
-        console.log(chalk.red("ERROR! The last step does not put you back on the starting planet."))
-        valid = false
-    }
-
-    if(!valid) {
-        console.log(chalk.bold.red("Please fix your step data and run again."))
-        process.exit(0)
-    }
-}
-
-/**
- * Ensure the planets.js file doesn't have obvious errors
- */
-function validatePlanets() {
-    // Innocent until proven guilty
-    let valid = true
-
-    // Make sure at least one planet has a restaurant
-    let foundRestaurant = false
-
-    for(let planet in planets) {
-        const planetData = planets[planet]
-
-        if(!planetData.toExchange) {
-            console.log(chalk.red(`ERROR! No path to exchange provided for planet ${planet}.`))
-            valid = false
-        }
-
-        if(!planetData.fromExchange) {
-            console.log(chalk.red(`ERROR! No path from exchange provided for planet ${planet}.`))
-            valid = false
-        }
-
-        if(!planetData.toLink) {
-            console.log(chalk.red(`ERROR! No path to link provided for planet ${planet}.`))
-            valid = false
-        }
-
-        if(!planetData.fromLink) {
-            console.log(chalk.red(`ERROR! No path from link provided for planet ${planet}.`))
-            valid = false
-        }
-
-        if(planetData.toRestaurant)
-            foundRestaurant = true
-    }
-
-    if(!foundRestaurant) {
-        console.log(chalk.red("ERROR! No planets have restaurants defined. Need at least one restaurant."))
-        valid = false
-    }
-
-    if(!valid) {
-        console.log(chalk.bold.red("Please fix your planet data and run again."))
-        process.exit(0)
     }
 }
 
