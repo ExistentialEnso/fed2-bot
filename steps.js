63b4e2182c1897209a960dd4299aacf28b668592
Thorne Melcher <existentialenso@gmail.com>
2020-03-22 06:39:53 -0400

    Slight output tweaks

--- steps.js
+++ steps.js
@@ -1,5 +1,9 @@
 /**
  * This file defines the steps that are taken each cycle the bot runs.
+ * 
+ * Each step needs a 'type' that is one of: TRADE, MOVE, WALK_UP
+ * 
+ * TRADE and MOVE steps need a 'to' and 'from' that are planet names defined in planets.js
  */
 
 const steps = [
